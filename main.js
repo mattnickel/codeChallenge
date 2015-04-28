2d986a5983a71b80f3a70358f87b0427fdcc0ca6
Matt Nickel <matt@nickelimages.com>
2015-04-27 21:48:16 -0700

    Add Javascript

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-//ingredients checklist line out
+
 $('span').on('click', function(e){
   $(this).toggleClass('unchecked');
   $(this).toggleClass('checked');
-  });//$(e.target.textcontent).toggleClass('.linethrough')
+  });
 $('li').on('click', function(){
   $(this).toggleClass('linethrough')
 });
